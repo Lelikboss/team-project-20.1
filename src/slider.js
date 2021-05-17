53094d171ab3353a2ce9a9e6a131a294e8499a39
Alexey Alexeew <lelikboss97@gmail.com>
2021-05-17 14:51:02 +0300

    slider

---
+++ src/slider.js
@@ -0,0 +1,9 @@
+$('#carousel').slick({
+    arrows: true,
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 3,
+    focusOnSelect: true,
+    autoplay: false,
+    speed: 3000
+});
