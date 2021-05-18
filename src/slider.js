3f43cb37f7504b7cfc5b503b8835bbbff3ff5abe
ivanartemenko <ivan.artemenko@masterofcode.com>
2021-05-18 15:50:29 +0300

    Fix carousel animation

--- src/slider.js
+++ src/slider.js
@@ -1,21 +1,46 @@
-$('#carousel').slick({
-    arrows: true,
+const ANIMATION_TIMING = 500;
+const $carouselEl = $('#carousel');
+const $reviewsEl = $('[data-reviews]');
+const $carouselRangeEl = $('[data-slider-range]');
+
+function renderCarouselRange(_, slick) {
+    $carouselRangeEl.text(`${slick.currentSlide + 1}/${slick.slideCount}`);
+}
+
+$carouselEl.on('init', renderCarouselRange);
+$carouselEl.slick({
     centerMode: true,
     centerPadding: '0px',
     slidesToShow: 3,
     focusOnSelect: true,
+    draggable: false,
     autoplay: false,
-    speed: 3000,
+    speed: ANIMATION_TIMING,
     responsive: [
         {
         breakpoint: 767.98,
             settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
             }
         }
-
     ]
+});
+
+$carouselEl.on('afterChange', renderCarouselRange);
+$carouselEl.on('beforeChange', (e, slick, currSlide, nextSlide) => {
+    if (currSlide === nextSlide) return;
+
+    $reviewsEl.addClass('reviews--animation');
+
+    $(`[data-slide]`).each((_, slideEl) => {
+        const $slideEl = $(slideEl);
+        const currentSlide = $slideEl.attr('data-slide');
+            
+        $slideEl.toggleClass('article-contant__text--active', +currentSlide === nextSlide);
+        setTimeout(() => {
+            $reviewsEl.removeClass('reviews--animation');
+        }, ANIMATION_TIMING);
+    });
 });
