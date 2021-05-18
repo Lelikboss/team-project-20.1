714ea471eac4f0b1f6dc4856dd6d50bc7bd7d564
Mark <79188082+Markus252525@users.noreply.github.com>
2021-05-18 12:32:31 +0300

    button_more

---
+++ src/read-more-about.js
@@ -0,0 +1,18 @@
+// function toggleTextAbout() {
+//     var pointsAbout =
+//         document.getElementById("pointsAbout");
+//     var showMoreTextAbout =
+//         document.getElementById("moreTextAbout");
+//     var buttonTextAbout =
+//         document.getElementById("textButtonAbout");
+//     if (pointsAbout.style.display === "none") {
+//         showMoreTextAbout.style.display = "none";
+//         pointsAbout.style.display = "inline";
+//         buttonTextAbout.innerHTML = "Read More";
+//     }
+//     else { 
+//         showMoreTextAbout.style.display = "inline";
+//         pointsAbout.style.display = "none";
+//         buttonTextAbout.innerHTML = "Less";
+//     }
+// }
---
+++ src/read-more-program.js
@@ -0,0 +1,24 @@
+// /..........................About.......................................//
+
+
+
+// ..............................................Program/.....................................//////////////////
+
+// function toggleText() {
+//     var pointsProgram =
+//         document.getElementById("pointsProgram");
+//     var showMoreTextProgram =
+//         document.getElementById("moreTextProgram");
+//     var buttonTextProgram =
+//         document.getElementById("textButtonProgram"); 
+//     if (pointsProgram.style.display === "none") {
+//         showMoreTextProgram.style.display = "none";
+//         pointsProgram.style.display = "inline";
+//         buttonTextProgram.innerHTML = "Read More";
+//     }
+//     else {
+//         showMoreTextProgram.style.display = "inline";
+//         pointsProgram.style.display = "none";
+//         buttonTextProgram.innerHTML = "Less";
+//     }
+// }
